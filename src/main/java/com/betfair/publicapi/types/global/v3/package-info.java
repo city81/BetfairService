39c2ce8066370e7fe9840530db472a450e661b9e
Geraint Jones <city81@btinternet.com>
2013-01-03 17:46:38 +0000

    Change to pom to generate Betfair code from wsdls

--- src/main/java/com/betfair/publicapi/types/global/v3/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.betfair.com/publicapi/types/global/v3/")
-package com.betfair.publicapi.types.global.v3;
--- src/main/java/com/betfair/publicapi/v3/bfglobalservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.betfair.com/publicapi/v3/BFGlobalService/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.betfair.publicapi.v3.bfglobalservice;
